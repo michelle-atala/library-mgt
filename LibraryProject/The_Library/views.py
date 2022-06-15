c3dc2095e29d1ed507d30c14c45b573c1264eda9
vnsemere <vnsemere@gmail.com>
2022-06-15 13:49:27 +0100

    Basic login view

--- LibraryProject/The_Library/views.py
+++ LibraryProject/The_Library/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.template import loader
+
+from django.http import HttpResponse
+def login (request):
+    template=loader.get_template("login.html")
+
+    return HttpResponse(template.render())
 
 
 # Create your views here.
