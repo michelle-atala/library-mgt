604d14c56c7f77b4861ac8a1f38001849b7be63c
Jerome Ssenyonjo <jeromessenyonjo@gmail.com>
2022-06-15 23:06:41 +0300

    Adjusted the search function (other than GET request) and included the search_result function

--- LibraryProject/The_Library/views.py
+++ LibraryProject/The_Library/views.py
@@ -1,34 +1,32 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render, redirect
 from .models import book
 from .forms import Book_search
 
 
-# # Create your views here.
-# def search(request, *args,**kwargs):
-#    # Michelle please uncomment the code when you need to see if the errors disappear after the fields creation.
-#
-#     if request.method == 'GET':                     #Checking if the request is GET to bind the user data to a fresh form.
-#             form = Book_search(request.GET)
-#
-#             if form.is_valid():
-#              title = request.cleaned_data['title']
-#              obj = list(book.objects.filter(title__icontains=title)) #Using contains to take care of word-spacing.
-#
-#              my_ctxt = {
-#                  "object": obj
-#              }
-#
-#             return redirect(index(request,my_ctxt))
-#
-#     else:                                           #Render a blank form for any other request.
-#             form = Book_search()
-#             my_ctxt = {
-#                 "form": form
-#             }
-#
-#             return render(request, "search.html", my_ctxt) # Redirect to the first page for an updated catalog.
-#
+# Create your views here.
+def search(request, *args, **kwargs):
+    # Michelle please uncomment the code when you need to see if the errors disappear after the fields creation.
+
+    if request.method == 'GET':  # Checking if the request is GET to bind the user data to a fresh form.
+        form = Book_search(request.GET)
+
+        if form.is_valid():
+            title = request.cleaned_data['title']
+            obj = list(book.objects.filter(title__icontains=title))  # Using contains to take care of word-spacing.
+
+            my_ctxt = {
+                "object": obj
+            }
+            return redirect(search_result(request, my_ctxt))
+
+
+
+    else:  # Render a 400 code
+
+        return HttpResponseBadRequest("<h1>{{request.method}} is not appropriate for this.")
+
+
 #  DON'T REMOVE THIS INDEX FUNCTION
 # def index(request, ctxt=None, *args, **kwargs):
 #      my_ctxt = None
@@ -54,4 +52,14 @@ def index(request):
 
     }
 
-    return render(request,'index.html', context)
+    return render(request, 'index.html', context)
+
+
+def search_result(request, ctxt):  # Display search results using index.html
+    title = ctxt.title
+    my_query = book.objects.filter(title=title)
+
+    context = {
+        "books": my_query
+    }
+    return render(request, 'index.html', context)
