130e242f135cab743f43e9d802fad6379dea7c9f
vnsemere <vnsemere@gmail.com>
2022-06-16 12:10:10 +0100

    created a new login form and login view

---
+++ LibraryProject/The_Library/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Student
+
+class SignUp_form(forms.Form):
+    first_name=forms.CharField(max_length=50)
+    last_name=forms.CharField(max_length=50)
+    user_name=forms.CharField(max_length=50)
+    email=forms.EmailField()
+    password=forms.CharField(max_length=50)
+
+class Login_form(forms.Form):
+    user_name=forms.CharField(max_length=50)
+    password=forms.CharField(max_length=50)
+
+def clean_data (self):
+    user_name=self.cleaned_data.get("user_name")
+    password=self.cleaned_data.get("password")
+
+    data=Student.objects.filter(user_name=user_name,password=password)
+    if not data :
+        raise forms.ValidationError("Username or password is incorrect")
+
+    return user_name,password
+
+
