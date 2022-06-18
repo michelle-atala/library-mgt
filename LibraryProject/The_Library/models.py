4d0e6d1c239f183efd3de576959a95a1987d6b77
Michelle.atala <atalamchll7@gmail.com>
2022-06-18 21:29:36 +0300

    Temporal required=false fields in forms.py
    Included search_result url in urls.py and search.html
    
    Modified search_result in views.py to cater for temporal use of subject_area field in books query.
    
    organised admin interface
    created database for borrowed books

--- LibraryProject/The_Library/models.py
+++ LibraryProject/The_Library/models.py
@@ -13,7 +13,16 @@ class book(models.Model):
     def __str__(self):
         return f"{self.title},{self.author},{self.shelf_number}"
 
+class borrowed_book(models.Model):
+    returned= models.BooleanField()
+    book_id=models.ForeignKey(book,on_delete=models.CASCADE)
+    student=models.CharField(max_length=100)
+    borrow_date=models.DateField()
+    return_date=models.DateField()
+    penalty_due=models.IntegerField()
 
+    def __str__(self):
+        return f"{self.student},{self.book_id},{self.borrow_date},{self.return_date},{self.returned},{self.penalty_due}"
 
 
 
