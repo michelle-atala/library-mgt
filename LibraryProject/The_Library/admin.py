128cb6e5da89bee60695dd32a3b3af5738c9cbc7
Michelle.atala <atalamchll7@gmail.com>
2022-06-18 16:10:53 +0300

    Temporal required=false fields in forms.py
    Included search_result url in urls.py and search.html
    
    Modified search_result in views.py to cater for temporal use of subject_area field in books query.
    
    organised admin interface

--- LibraryProject/The_Library/admin.py
+++ LibraryProject/The_Library/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import book
-admin.site.register(book)
+
+@admin.register(book)
+class bookAdmin(admin.ModelAdmin):
+    list_display = ("title","author","shelf_number")
 # Register your models here.
